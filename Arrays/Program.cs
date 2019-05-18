e94278da25ab8c8be1924e7570692efc9f42787c
eversonsa10 <eversonsa2014@gmail.com>
2019-05-18 18:08:05 -0300

    estudo sintaxe array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,7 +10,25 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("teste");
+            //Array com 5 posição
+            int[] idade = new int[5];
+            idade[0] = 18;
+            idade[1] = 42;
+            idade[2] = 65;
+            idade[3] = 29;
+            idade[4] = 38;
+
+            int acumulador = 0;
+            for (int indice = 0; indice <= 4; indice++)
+            {
+                int id = idade[indice];
+                Console.WriteLine($" idade[{indice}]");
+                acumulador += id;
+            }
+
+            double media = acumulador / 5;
+            Console.WriteLine("Media da idade: " + media);
+            Console.ReadLine();
         }
     }
 }
